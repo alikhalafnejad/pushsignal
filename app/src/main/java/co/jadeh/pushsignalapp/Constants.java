634f51060df99583c7c710eacb0ee3ed6fb1710d
jadehco <jadehapp@gmail.com>
2019-11-27 17:55:15 +0330

    add files

---
+++ app/src/main/java/co/jadeh/pushsignalapp/Constants.java
@@ -0,0 +1,13 @@
+package co.jadeh.pushsignalapp;
+
+public class Constants {
+
+    //also set this app_id in app/build.gradle into onesignal_app_id
+    public final static String APP_ID = "app-id";
+
+    //jadeh api-key
+    public final static String API_KEY = "jadeh-api-key";
+
+    //you can set this google_number in app/build.gradle into onesignal_google_project_number
+    public final static String GOOGLE_NUMBER = "1234567";
+}
